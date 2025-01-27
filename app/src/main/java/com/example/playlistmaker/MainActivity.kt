9e62523fc7f377b287cd595f52458e16fb5b8ed7
mr.friderick <mr.friderick@ya.ru>
2025-01-27 12:39:50 +0300

    Добавил новые скрины и обработку переходов

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -14,30 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.screen_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Обработка кнопки Поиск через анонимный класс
-        val buttonSearch = findViewById<Button>(R.id.search)
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Выполняю поиск!", Toast.LENGTH_SHORT).show()
-            }
-        }
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
-
-        // Обработка кнопки Медиатека через лямбда-выражение
-        val buttonMedia = findViewById<Button>(R.id.media)
-        buttonMedia.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Открываю медиатеку!", Toast.LENGTH_SHORT).show()
+        findViewById<Button>(R.id.search).setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
         }
 
-        // Обработка кнопки Настройки через лямбда-выражение
+        findViewById<Button>(R.id.media).setOnClickListener {
+            startActivity(Intent(this, MediaActivity::class.java))
+        }
+
         findViewById<Button>(R.id.settings).setOnClickListener {
-            Toast.makeText(this@MainActivity, "Открываю настройки!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.screen_settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<ImageView>(R.id.settings_back).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
