bc7462f1cbf60ba70ed5c516aefe2cd6b7328c01
mr.friderick <mr.friderick@ya.ru>
2025-02-09 12:40:38 +0300

    Добавлены обработчики для экрана Настройки

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +26,33 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        findViewById<MaterialTextView>(R.id.share).setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.text_for_share))
+            }
+            startActivity(Intent.createChooser(intent, "Поделиться через"))
+        }
+
+        findViewById<MaterialTextView>(R.id.support).setOnClickListener {
+            val recipient = getString(R.string.email_for_support)
+            val subject = getString(R.string.subject_for_support)
+            val body = getString(R.string.text_for_support)
+
+            val intent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:$recipient?subject=${Uri.encode(subject)}&body=${Uri.encode(body)}")
+            }
+            startActivity(intent)
+        }
+
+        findViewById<MaterialTextView>(R.id.agreement).setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.link_for_agreement))
+            }
+            startActivity(intent)
+        }
     }
 }
