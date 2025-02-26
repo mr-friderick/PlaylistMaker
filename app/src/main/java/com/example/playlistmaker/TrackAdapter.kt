c95f73710ae5ee67314337c5bdbfbdd79db134b5
mr.friderick <mr.friderick@ya.ru>
2025-02-26 16:44:24 +0300

    Добавлен список треков на экране поиска через RecyclerView

---
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.playlistmaker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackAdapter(private val tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        return TrackViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int = tracks.size
+}
