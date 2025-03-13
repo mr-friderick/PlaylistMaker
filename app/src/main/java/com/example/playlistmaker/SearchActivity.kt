693263f9a1383ab531801ba50b72da1119b03004
Pavel Vinogradov <69008871+mr-friderick@users.noreply.github.com>
2025-03-13 12:57:02 +0300

    ДЗ Спринт #11
    * Загрузка данных из apple
    
    * Выполнены основные пункты ТЗ
    
    * Небольшие правки оформления
    
    * Реализация кнопки Обновить + финальные штрихи
    
    * Правки после ревью
    
    * Правки после ревью #2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,28 +5,40 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 
 class SearchActivity : AppCompatActivity() {
     companion object {
         const val INPUT_SEARCH_TEXT = "INPUT_SEARCH_TEXT"
         const val INPUT_SEARCH_TEXT_DEF = ""
+        const val BASE_URL_SEARCH = "https://itunes.apple.com/"
     }
 
     private var inputText = INPUT_SEARCH_TEXT_DEF
     private lateinit var editText: EditText
     private lateinit var buttonClear: ImageView
+    private lateinit var buttonRefresh: MaterialButton
     private lateinit var toolbar: Toolbar
     private lateinit var recyclerView: RecyclerView
+    private lateinit var notFoundPlaceholder:  LinearLayout
+    private lateinit var failurePlaceholder: LinearLayout
+
+    private val apiService = RetrofitFactory.create(BASE_URL_SEARCH).create<ItunesAPI>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +50,6 @@ class SearchActivity : AppCompatActivity() {
         setListeners()
         processInstanceState(savedInstanceState)
         setFocusScreen()
-        createRecyclerView()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -62,16 +73,32 @@ class SearchActivity : AppCompatActivity() {
     private fun initScreenView() {
         editText = findViewById(R.id.search_edit_text)
         buttonClear = findViewById(R.id.search_clear_icon)
+        buttonRefresh = findViewById(R.id.button_refresh)
         toolbar = findViewById(R.id.search_back)
         recyclerView = findViewById(R.id.search_recycler_view)
+        notFoundPlaceholder = findViewById(R.id.search_not_found_placeholder)
+        failurePlaceholder = findViewById(R.id.search_failure_placeholder)
     }
 
     private fun setListeners() {
         editText.addTextChangedListener(TextWatcher())
 
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchSongs(editText.text.toString())
+            }
+            false
+        }
+        
         buttonClear.setOnClickListener {
+            createRecyclerView(arrayListOf())
+            switchVisibilityView(SearchStatus.GOOD)
             editText.setText("")
             editText.clearFocus()
+        }
+
+        buttonRefresh.setOnClickListener {
+            searchSongs(editText.text.toString())
         }
 
         toolbar.setNavigationOnClickListener {
@@ -79,6 +106,50 @@ class SearchActivity : AppCompatActivity() {
             intent.putExtra(INPUT_SEARCH_TEXT, inputText)
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun searchSongs(text: String) {
+        apiService.search(text)
+            .enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                    if (response.isSuccessful) {
+                        val songs = response.body()?.results ?: arrayListOf()
+                        if (songs.isNotEmpty()) {
+                            switchVisibilityView(SearchStatus.GOOD)
+                            createRecyclerView(songs)
+                        } else {
+                           switchVisibilityView(SearchStatus.NOT_FOUND)
+                        }
+
+                    } else {
+                        switchVisibilityView(SearchStatus.FAILURE)
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    switchVisibilityView(SearchStatus.FAILURE)
+                }
+            })
+    }
+
+    private fun switchVisibilityView(status: SearchStatus) {
+        when (status) {
+            SearchStatus.GOOD -> {
+                recyclerView.visibility = View.VISIBLE
+                notFoundPlaceholder.visibility = View.GONE
+                failurePlaceholder.visibility = View.GONE
+            }
+            SearchStatus.NOT_FOUND -> {
+                recyclerView.visibility = View.GONE
+                notFoundPlaceholder.visibility = View.VISIBLE
+                failurePlaceholder.visibility = View.GONE
+            }
+            else -> {
+                recyclerView.visibility = View.GONE
+                notFoundPlaceholder.visibility = View.GONE
+                failurePlaceholder.visibility = View.VISIBLE
+            }
         }
     }
 
@@ -93,44 +164,9 @@ class SearchActivity : AppCompatActivity() {
         editText.requestFocus()
     }
 
-    private fun createRecyclerView() {
-        val trackAdapter = TrackAdapter(tracksList())
+    private fun createRecyclerView(tracksList: ArrayList<Track>) {
+        val trackAdapter = TrackAdapter(tracksList)
         recyclerView.adapter = trackAdapter
-    }
-
-    private fun tracksList(): ArrayList<Track> {
-        return arrayListOf(
-            Track(
-                trackName = "Smells Like Teen Spirit",
-                artistName = "Nirvana",
-                trackTime = "5:01",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Billie Jean",
-                artistName = "Michael Jackson",
-                trackTime = "4:35",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Stayin' Alive",
-                artistName = "Bee Gees",
-                trackTime = "4:10",
-                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Whole Lotta Love",
-                artistName = "Led Zeppelin",
-                trackTime = "5:33",
-                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Sweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg "
-            )
-        )
     }
 
     private fun TextWatcher(): TextWatcher = object : TextWatcher {
@@ -143,7 +179,12 @@ class SearchActivity : AppCompatActivity() {
         }
 
         override fun afterTextChanged(s: Editable?) {
-            inputText = s.toString()
+            val currentText = s.toString()
+            if (currentText.isEmpty()) {
+                createRecyclerView(arrayListOf())
+                switchVisibilityView(SearchStatus.GOOD)
+            }
+            inputText = currentText
         }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SearchStatus.kt
@@ -0,0 +1,5 @@
+package com.example.playlistmaker
+
+enum class SearchStatus {
+    GOOD, NOT_FOUND, FAILURE
+}
