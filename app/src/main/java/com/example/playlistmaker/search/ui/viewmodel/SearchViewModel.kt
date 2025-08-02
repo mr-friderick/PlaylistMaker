bb31c74d91b054eff5f012ded22391e309884d1c
mr-friderick <mr.friderick@ya.ry>
2025-08-02 12:40:05 +0300

    Перевел старт поиска и клик на корутины

--- app/src/main/java/com/example/playlistmaker/search/ui/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/viewmodel/SearchViewModel.kt
@@ -3,14 +3,20 @@ package com.example.playlistmaker.search.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.playlistmaker.search.domain.interactors.HistoryInteractor
 import com.example.playlistmaker.search.domain.interactors.TracksInteractor
 import com.example.playlistmaker.search.domain.models.Track
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchViewModel(
     private val tracksInteractor: TracksInteractor,
     private val historyInteractor: HistoryInteractor
 ): ViewModel() {
+
+    private var searchJob: Job? = null
 
     private val state = MutableLiveData<SearchViewState>(SearchViewState.Default)
     val stateLiveData: LiveData<SearchViewState> = state
@@ -36,26 +42,38 @@ class SearchViewModel(
         state.postValue(SearchViewState.History(historyInteractor.read()))
     }
 
-    fun searchTracks(expression: String) {
+    fun searchTracks(expression: String, needDelay:Boolean = true) {
         if (expression.isEmpty()) return
 
-        state.postValue(SearchViewState.Loading)
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            if (needDelay) {
+                delay(SEARCH_DEBOUNCE_DELAY)
+            }
 
-        tracksInteractor.searchTracks(
-            expression,
-            object : TracksInteractor.TracksConsumer {
-                override fun consume(foundTracks: ArrayList<Track>, isError: Boolean) {
-                    if (foundTracks.isEmpty()) {
-                        if (isError) {
-                            state.postValue(SearchViewState.Error)
+            state.postValue(SearchViewState.Loading)
+
+            tracksInteractor.searchTracks(
+                expression,
+                object : TracksInteractor.TracksConsumer {
+                    override fun consume(foundTracks: ArrayList<Track>, isError: Boolean) {
+                        if (foundTracks.isEmpty()) {
+                            if (isError) {
+                                state.postValue(SearchViewState.Error)
+                            } else {
+                                state.postValue(SearchViewState.NotFound)
+                            }
                         } else {
-                            state.postValue(SearchViewState.NotFound)
+                            state.postValue(SearchViewState.Content(foundTracks))
                         }
-                    } else {
-                        state.postValue(SearchViewState.Content(foundTracks))
                     }
                 }
-            }
-        )
+            )
+        }
+
+    }
+
+    companion object {
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
 }
