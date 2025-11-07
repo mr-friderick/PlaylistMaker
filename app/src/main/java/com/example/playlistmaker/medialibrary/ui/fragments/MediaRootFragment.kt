faffcbfe9b794f878784c157fd1a69008f180dad
Pavel Vinogradov <mr.friderick@ya.ru>
2025-11-07 16:20:21 +0300

    Мини-правки по главному экрану медиатеки

--- app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragments/MediaRootFragment.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/fragments/MediaRootFragment.kt
@@ -6,53 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import com.example.playlistmaker.R
-import com.example.playlistmaker.databinding.FragmentMediaRootBinding
-import com.example.playlistmaker.medialibrary.ui.adapter.MediaViewPagerAdapter
-import com.google.android.material.tabs.TabLayoutMediator
 import com.example.playlistmaker.medialibrary.ui.compose.MediaRootScreen
 
 class MediaRootFragment : Fragment() {
-
-    private lateinit var binding: FragmentMediaRootBinding
-    private lateinit var adapter: MediaViewPagerAdapter
-    private lateinit var tabMediator: TabLayoutMediator
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMediaRootBinding.inflate(inflater, container, false)
-        //return binding.root
         return ComposeView(requireContext()).apply {
             setContent {
                 MediaRootScreen()
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        tabMediator.detach()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initVariables()
-    }
-
-    private fun initVariables() {
-        adapter = MediaViewPagerAdapter(this)
-        binding.viewPager.adapter = adapter
-
-        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> tab.text = getString(R.string.favorites_track_title)
-                1 -> tab.text = getString(R.string.playlist_title)
-            }
-        }
-        tabMediator.attach()
     }
 }
